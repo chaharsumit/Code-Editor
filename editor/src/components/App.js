0ac4cc9f1e46e5f4c067ce957fcae5c95cdcac89
Sumit Chahar <chahar.sumit888@gmail.com>
2022-02-24 11:09:59 +0530

    Complete live code editor

---
+++ editor/src/components/App.js
@@ -0,0 +1,45 @@
+import React, { createRef } from "react";
+import { EditorState, EditorView, basicSetup } from "@codemirror/basic-setup";
+import { javascript } from "@codemirror/lang-javascript";
+import Header from "./Header";
+import Editor from './Editor';
+import Preview from './Preview';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      xml: '<h1>Hello World!</h1>',
+      css: '',
+    }
+  }
+
+  handleChange = (value, language) => {
+    this.setState({ [language] : value })
+  }
+
+  render() {
+    let { xml, css } = this.state;
+    return (
+      <>
+        <Header />
+        <div className="main-body container flex">
+          <div className="code-container flex">
+            <Editor 
+              language='xml'
+              value={xml}
+              displayLabel='html'
+              handleChange={this.handleChange}
+            />
+            <Editor 
+              language='css'
+              value={css}
+              handleChange={this.handleChange}
+            />
+          </div>
+          <Preview xml={xml} css={css} />
+        </div>
+      </>
+    );
+  }
+}
---
+++ editor/src/components/Header.js
@@ -0,0 +1,8 @@
+export default function Header(){
+  return (
+    <div className="header container">
+      <h1 className="text-xlg bold">Code Editor</h1>
+      <p className="text-md">You can write and preview HTML and CSS!</p>
+    </div>
+  )
+}
